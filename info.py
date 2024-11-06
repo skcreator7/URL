ce2b6ef768544536ae2f14a7c2a255fe86349198
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-11-06 23:05:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
+API_ID       = ""
 API_HASH     = ""
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+LOG_CHANNEL  = ""
+ADMIN        = ""
+CHANNEL      = "@skcreator7"
