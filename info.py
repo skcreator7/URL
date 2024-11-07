80dcef92862c6b9e198713f724d4fa0f1e0020c0
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-11-07 06:59:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 23978272
 API_HASH     = "c3881f07b3bfd6b1d7ab3a400d840a28"
 BOT_TOKEN    = "7931402157:AAGn1TnJYKBWQpG_zqYewWEXvK-789lQ4w4"
-SESSION      = 
+SESSION      = ""
 DATABASE_URI = "mongodb+srv://skmovieswala7:SK6307@cluster0.wx6mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001603714460
 ADMIN        = 5928972764
