44146cc67d39a5ef79fc5b137894161525ad1024
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-11-07 06:29:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from client import Bot
+# main.py
+from flask import Flask
+app = Flask(__name__)
 
-print("Bot Started 💥")
-Bot().run()
+@app.route('/')
+def home():
+    return "Hello, World!"
 
+if __name__ == "__main__":
+    app.run()
